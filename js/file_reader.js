d1dd403329ded5f1d6bb775b0895968063d4c6e3
Ivan Glushkov <gis.glushkov@yandex.ru>
2021-02-07 18:12:15 +0300

    Initial commit

---
+++ js/file_reader.js
@@ -0,0 +1,53 @@
+let fullText = "";
+let dataForChartAll = [];
+let dataForChart = [];
+let sizePointOnChart = 70000;
+
+function readFile(input) {
+    let file = input.files[0];
+
+    let reader = new FileReader();
+
+    reader.readAsText(file);
+
+    reader.onload = function () {
+        fullText = reader.result;
+        prepareDataForChart();
+        chartStart();
+    };
+
+    reader.onerror = function () {
+        console.log(reader.error);
+    };
+}
+
+function prepareDataForChart() {
+    let arrayText = fullText.split('\n');
+
+    let arraySplit = arrayText.map(function (element) {
+        return element.split(' ');
+    });
+
+    // проходит по значениям
+    for (let element of arraySplit) {
+        dataForChartAll.push({
+                "date": element[1] + ' ' + element[2],
+                "value": element[3]
+            }
+        );
+    }
+
+    limitDataForChart();
+}
+
+function limitDataForChart() {
+    let startPoint = dataForChartAll.length - sizePointOnChart;
+
+    if (startPoint < 0) {
+        dataForChart = dataForChartAll
+        console.log(dataForChart.length)
+    } else {
+        dataForChart = dataForChartAll.slice(startPoint, dataForChartAll.length)
+        console.log(dataForChart.length)
+    }
+}
